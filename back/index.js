6fd07fce1cee82c758d60aed0c75687c01156e1e
Shailee Eliyahu <shailee.eliyahu@gmail.com>
2022-04-23 21:37:18 +0300

    prep for CD

---
+++ back/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+/*----------------*\
+|   Backend App    |
+\-----------------*/
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 4001;
+app.listen(PORT, () => console.log(`Listening for requests on port ${PORT}...`));
+
+/*-----------------*\
+|   Frontend App    |
+\------------------*/
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
