e93deb108311b0d20ba0bf22e436b5647837b65f
Shailee Eliyahu <shailee.eliyahu@gmail.com>
2022-04-23 22:40:33 +0300

    created poc github action 1

--- back/index.js
+++ back/index.js
@@ -19,6 +19,6 @@ app.listen(PORT, () => console.log(`Listening for requests on port ${PORT}...`))
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/weird/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
